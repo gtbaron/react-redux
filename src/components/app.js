dccd68965749e68d059b1d5ace97612dd1ae8e6a
Mike <sanders.michael.j@gmail.com>
2018-10-02 14:02:04 -0400

    added react logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,22 @@
+import "../index.css";
 import React from 'react';
+import logo from "../logo.svg";
 import List from "./list";
 import Form from "./form";
 
 const App = () => (
     <div>
         <div className="row mt-5 text-center">
-            <h1 className="w-100">Using: React & Redux</h1>
+            <div className="margin-auto">
+                <div className="d-inline-block">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </div>
+                <div className="d-inline-block height-30">
+                    <div className="d-table-cell">
+                        <h1 className="w-100">Using: React & Redux</h1>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="row mt-5">
             <div className="col-md-4 offset-md-1">
